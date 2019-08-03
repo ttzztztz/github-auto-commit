addcac7b4baf55271804d06fe40003fe6337e6ea
ttzztztz <935319340@qq.com>
2019-08-03 16:18:37 +0800

    Update Cpp Files

--- main.cpp
+++ main.cpp
@@ -5,31 +5,7 @@
 #include <ctime>
 #include <sstream>
 
-// get system() call stdout function from https://oomake.com/question/307885
-char *exec(const char *command) {
-    FILE *fp;
-    char *result = nullptr;
-    size_t len = 0;
-    fflush(nullptr);
-    fp = popen(command, "r");
-    if (fp == nullptr) {
-        printf("Cannot execute command:\n%s\n", command);
-        return nullptr;
-    }
-    while (getline(&result, &len, fp) != -1) {
-        fputs(result, stdout);
-    }
-    free(result);
-    fflush(fp);
-    if (pclose(fp) != 0) {
-        perror("Cannot close stream.\n");
-    }
-    return result;
-}
-
 int main() {
-    std::string repo = getenv("GIT_REPO");
-
     std::cout << "Init global config..." << std::endl;
     while (true) {
         std::fstream rand_stream;
@@ -44,7 +20,7 @@ int main() {
         rand_stream.close();
 
         std::cout << timestamp << " will push to remote repo." << std::endl;
-        std::cout << exec("cd /home/repo && git add . && git commit -m \"daily commit\" && git push -u origin master")
+        std::cout << system("cd /home/repo && git add . && git commit -m \"daily commit\" && git push -u origin master")
                   << std::endl;
         sleep(24 * 60 * 60);
     }
